c218ede12c5f5472c4500eb8b1aa1f50f3d06e92
Lamy210 <syoytaku@gmail.com>
2023-12-04 02:06:41 +0900

    SpringBoot設定

---
+++ src/test/java/com/example/api/db/group/DBService/DbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.db.group.DBService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
